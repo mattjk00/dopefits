6ecb83c0c396a327ebb2cbedebededfc25cfbaa4
Matthew K <mattjk@outlook.com>
2015-01-03 16:31:41 -0500

    added option to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,21 @@ class CommentsController < ApplicationController
 	
 	def create
 		@fit = Fit.find(params[:fit_id])
-		@comment = @fit.comments.create(comment_params)
 		
-		@comment.update_attributes(:poster =>  session[:current_user_id])
+		if session[:current_user_id] != nil
+			@comment = @fit.comments.create(comment_params)
+			@comment.update_attributes(:poster =>  session[:current_user_id])
+			redirect_to fit_path(@fit)
+		else
+			flash[:notice] = "You need an account in order to comment"
+			redirect_to fit_path(@fit)
+		end
+	end
+	
+	def destroy
+		@fit = Fit.find(params[:fit_id])
+		@comment = @fit.comments.find(params[:id])
+		@comment.destroy
 		redirect_to fit_path(@fit)
 	end
 	
